a8af9c56c9ab92db6444b96325f3aa428abb0227
shrenrip <100356541+shrenrip@users.noreply.github.com>
2026-03-09 20:20:31 +0545

    initial commit

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        count: 127 
-    },
-    priceCents: 2095
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
-
 let productsHTML = ``;
 products.forEach((product) => {
     productsHTML += `
@@ -73,12 +45,15 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart-button">
             Add to Cart
           </button>
     </div>`;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+const addtoCart = document.querySelector('.js-add-to-cart-button');
+addtoCart.addEventListener('click', () => {
+  console.log("added to cart");
+})
