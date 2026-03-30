47c6070f9aa728e5655aebb20f4add627ee4c24a
shrenrip <100356541+shrenrip@users.noreply.github.com>
2026-03-30 22:45:13 +0545

    feat: used inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -33,9 +33,30 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return ``;
+  }
 }
 
-export const products = [
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+     return `
+     <a href="${this.sizeChartLink}" target="_blank">
+      Size chart
+     </a>
+     `;
+  }
+}
+
+export const products = [ 
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -695,5 +716,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
