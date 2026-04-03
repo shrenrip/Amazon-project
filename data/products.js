229cf7a24beb178c8a97b2c0287437e3b30c63dc
shrenrip <100356541+shrenrip@users.noreply.github.com>
2026-04-03 17:25:16 +0545

    feat: convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -58,6 +58,25 @@ class Clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+
+
 export function loadProducts(func) {
   const xhr = new XMLHttpRequest();
 
@@ -77,6 +96,8 @@ export function loadProducts(func) {
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+
+
 
 
 /*
